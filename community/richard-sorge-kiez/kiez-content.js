da448c0c65b94f5cc32e218f395a41774f863fab
Leni-Rsk <heli.russak@yahoo.de>
2026-09-03 16:02:23 +0200

    Add new event 'Hoffest 2026' to KIEZ_CONTENT

--- community/richard-sorge-kiez/kiez-content.js
+++ community/richard-sorge-kiez/kiez-content.js
@@ -4,7 +4,18 @@
  */
 window.KIEZ_CONTENT={
   events:[
-    {date:"Bald",title:"Erster Kiez-Termin",text:"Hier kann Leni den ersten gemeinsamen Termin eintragen.",interest:"Ich hätte Lust"}
+    {
+      date: "Sommer 2026",
+      title: "Hoffest 2026",
+      text: "Ein gemeinsames Fest für alle Nachbarinnen und Nachbarn im Innenhof. Es gibt Musik, Mitbring-Buffet und Zeit zum Kennenlernen und Quatschen.",
+      interest: "Ich bin dabei!"
+    },
+    {
+      date: "Bald",
+      title: "Erster Kiez-Termin",
+      text: "Hier kann Leni den ersten gemeinsamen Termin eintragen.",
+      interest: "Ich hätte Lust"
+    }
   ],
   knowledge:[
     {title:"Wartungsplan",text:"Regelmäßige Hausaufgaben sammeln wir hier: zum Beispiel Ventile bewegen und sichtbare Schäden früh melden."},
